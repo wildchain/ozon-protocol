3156f2fca5fed18b4548c024d63a05b473f7a0e0
bristinWild <bristin@mysocialmotion.org>
2025-10-15 05:04:18 +0530

    program fixes and events added

--- programs/avs-oracle/src/lib.rs
+++ programs/avs-oracle/src/lib.rs
@@ -4,7 +4,7 @@ use restaking_programs::cpi::accounts::SlashOperator;
 use restaking_programs::program::RestakingPrograms;
 use restaking_programs::{OperatorAccount, OperatorVault, RewardTreasury};
 
-declare_id!("HaTaUi4UhuLt9MzjW2Avss9WdxKJjsoFU6MX7BoiAdAa");
+declare_id!("7VMoS2bsEUHyJyMogfn3ZZ5t4bMMiFpggtZuujEVTVRN");
 
 #[program]
 pub mod avs_oracle {
