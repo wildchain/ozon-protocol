7dad944dbcf3ecdaa787ee55eb4b6bbb9fee16c2
bristinWild <bristin@mysocialmotion.org>
2025-10-19 01:57:39 +0530

    root-fixes

--- programs/avs-oracle/src/lib.rs
+++ programs/avs-oracle/src/lib.rs
@@ -4,7 +4,7 @@ use restaking_programs::cpi::accounts::SlashOperator;
 use restaking_programs::program::RestakingPrograms;
 use restaking_programs::{OperatorAccount, OperatorVault, RewardTreasury};
 
-declare_id!("6NLkSfQvmRgsbW8ywJLCbnnVh1nYTg5xfjSeM5E7YhcU");
+declare_id!("122iaw5CsWYKCpZFJg5DwjUtp8zyoeLFanreuwKHLzFV");
 
 #[program]
 pub mod avs_oracle {
--- programs/restaking-programs/src/lib.rs
+++ programs/restaking-programs/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_spl::token::{self, Burn, Mint, MintTo, Token, TokenAccount, Transfer}
 
 // This is your program's public key and it will update
 // automatically when you build the project.
-declare_id!("G9HUZQDnpJsFHST2KG56CkmcLWHrMrBB7XNRyZ9vR51a");
+declare_id!("E8SwyhYcBCWDJu6QM8Mcaguo8wP676wZ3yDzrrvm5yWP");
 
 #[program]
 pub mod restaking_programs{
